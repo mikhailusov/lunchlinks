f02fcc639932fed97c0e1a1fba9388d17db549f4
Kimberly Yu <kimberly.yu@appdynamics.com>
2019-02-07 15:14:06 -0800

    print payload

--- index.js
+++ index.js
@@ -430,7 +430,7 @@ app.post('/optin', (req, res) => {
 app.post('/actions', (req, res) => {
   const payload = JSON.parse(req.body.payload);
   const {type, user, submission} = payload;
-  console.log(submission);
+  console.log(payload);
   if (type === 'dialog_submission') {
     var interest1 = submission.interest1;
     var interest2 = submission.interest2;
