d9d268cd76dcd918741a7fb40f0e18a006abce5b
Mikhail Usov <mikhail.usov@appdynamics.com>
2019-02-06 17:25:20 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Easy-Slack-Tutorial
---
+++ index.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require("request");
+
+// Creates express app
+const app = express();
+// The port used for Express server
+const PORT = 3000;
+// Starts server
+app.listen(process.env.PORT || PORT, function() {
+  console.log('Bot is listening on port ' + PORT);
+});
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.post('/', (req, res) => {
+  var data = {form: {
+    token: process.env.SLACK_AUTH_TOKEN,
+    channel: "#lunchlinks",
+    text: "Hi! :wave: \n I'm your new bot."
+  }};
+  request.post('https://slack.com/api/chat.postMessage', data, function (error, response, body) {
+    // Sends welcome message
+    res.json();
+  });
+});
