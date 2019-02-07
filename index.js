e1cf200a2ea245d0e8bbe6f23f951310f87f56bf
Mikhail Usov <mikhail.usov@appdynamics.com>
2019-02-07 10:59:34 -0800

    removing slack sdk

--- index.js
+++ index.js
@@ -3,17 +3,6 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require("request");
-const { WebClient} = require('@slack/client');
-const token = process.env.SLACK_AUTH_TOKEN;
-const web = new WebClient(token);
-
-// const conversationId = 'CFZ3JKN2F'; // lunchlinks channel
-// web.chat.postMessage({ channel: conversationId, text: 'Testing postMessage' })
-//   .then((res) => {
-//     // `res` contains information about the posted message
-//     console.log('Message sent: ', res.ts);
-//   })
-//   .catch(console.error);
 
 // Creates express app
 const app = express();
@@ -40,183 +29,35 @@ app.post('/', (req, res) => {
   });
 });
 
-app.post('/signup', (req, res) => {
-  return web.chat.postMessage({
-      channel: res.user_id,
-      text: "To help me match you with other AppDynamos, please select all the interests you would like to be matched on.",
-      attachments: [
-        {
-          "text": "Food",
-          "fallback": "You are unable to select an interest",
-          "callback_id": "interests",
-          "color": "#3AA3E3",
-          "attachment_type": "default",
-          "actions": [
-            {
-              "name": "interest",
-              "text": "boba",
-              "type": "button",
-              "value": "boba"
-            },
-            {
-              "name": "interest",
-              "text": "coffee",
-              "type": "button",
-              "value": "coffee"
-            }
-          ]
-        },
-        {
-          "text": "Pets",
-          "fallback": "You are unable to select an interest",
-          "callback_id": "interests",
-          "color": "#3AA3E3",
-          "attachment_type": "default",
-          "actions": [
-            {
-              "name": "interest",
-              "text": "cats",
-              "type": "button",
-              "value": "cats"
-            },
-            {
-              "name": "interest",
-              "text": "dogs",
-              "type": "button",
-              "value": "dogs"
-            }
-          ]
-        },
-        {
-          "text": "Hobbies",
-          "fallback": "You are unable to select an interest",
-          "callback_id": "interests",
-          "color": "#3AA3E3",
-          "attachment_type": "default",
-          "actions": [
-            {
-              "name": "interest",
-              "text": "fire dancing",
-              "type": "button",
-              "value": "fire_dancing"
-            },
-            {
-              "name": "interest",
-              "text": "photography",
-              "type": "button",
-              "value": "photography"
-            },
-            {
-              "name": "interest",
-              "text": "pixar",
-              "type": "button",
-              "value": "pixar"
-            },
-            {
-              "name": "interest",
-              "text": "reading",
-              "type": "button",
-              "value": "reading"
-            },
-            {
-              "name": "interest",
-              "text": "ultimate frisbee",
-              "type": "button",
-              "value": "ultimate_frisbee"
-            },
-            {
-              "name": "interest",
-              "text": "video games",
-              "type": "button",
-              "value": "boba"
-            },
-            {
-              "name": "interest",
-              "text": "volunteering",
-              "type": "button",
-              "value": "volunteering"
-            },
-            {
-              "name": "interest",
-              "text": "watches",
-              "type": "button",
-              "value": "watches"
-            },
-            {
-              "name": "interest",
-              "text": "wine tasting",
-              "type": "button",
-              "value": "wine_tasting"
-            },
-            {
-              "name": "interest",
-              "text": "wrestling",
-              "type": "button",
-              "value": "wrestling"
-            }
-          ]
-        },
-        {
-          "text": "Professional skills",
-          "fallback": "You are unable to select an interest",
-          "callback_id": "interests",
-          "color": "#3AA3E3",
-          "attachment_type": "default",
-          "actions":[
-            {
-              "name": "interest",
-              "text": "ML/AI",
-              "type": "button",
-              "value": "ML/AI"
-            },
-            {
-              "name": "interest",
-              "text": "public speaking",
-              "type": "button",
-              "value": "public_speaking"
-            }
-          ]
-        }
-      ]
-  })
-  .then((res) => {
-    console.log('Message posted!');
-    console.log(res.body);
-  })
-  .catch(console.error);
-})
-
 app.post('/optin', (req, res) => {
   var data = {
-    trigger_id: req.trigger_id,
-    dialog: {
-      callback_id: "ryde-46e2b0",
-      title: "Request a Ride",
-      submit_label: "Request",
-      notify_on_cancel: true,
-      state: "Limo",
-      elements: [
-          {
-              type: "text",
-              label: "Pickup Location",
-              name: "loc_origin"
-          },
-          {
-              type: "text",
-              label: "Dropoff Location",
-              name: "loc_destination"
-          }
-      ]
-    }
-  };
-
+  trigger_id: req.trigger_id,
+  dialog: {
+    callback_id: "ryde-46e2b0",
+    title: "Request a Ride",
+    submit_label: "Request",
+    notify_on_cancel: true,
+    state: "Limo",
+    elements: [
+        {
+            type: "text",
+            label: "Pickup Location",
+            name: "loc_origin"
+        },
+        {
+            type: "text",
+            label: "Dropoff Location",
+            name: "loc_destination"
+        }
+    ]
+  }
+};
 var headers = {
     'Content-Type' : 'application/json',
     'Authorization': process.env.SLACK_AUTH_TOKEN
 };
-request.post({url: 'https://slack.com/api/dialog.open', form: data, headers: headers}, function (error, response, body) {
-  res.json();
-  console.log(response.body)
-});
-
+  request.post({url: 'https://slack.com/api/dialog.open', form: data, headers: headers}, function (error, response, body) {
+    res.json();
+    console.log(response.body)
+  });
 });
