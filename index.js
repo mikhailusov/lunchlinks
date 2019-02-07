eeecc940f87f589f659d7d6f9441605f0f628ec9
Mikhail Usov <mikhail.usov@appdynamics.com>
2019-02-07 12:52:12 -0800

    optin fix

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ app.post('/optin', (req, res) => {
         }
     ]
   };
-  console.log(req);
+  console.log(req.body.trigger_id);
   var data = {
   trigger_id: req.trigger_id,
   dialog: JSON.stringify(form)
