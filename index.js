ba478c8807344f5c4468899b024c09b6de0bdb79
Mikhail Usov <mikhail.usov@appdynamics.com>
2019-02-06 18:24:43 -0800

    adding /optin endpoint

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ app.post('/', (req, res) => {
 
 app.post('/optin', (req, res) => {
   var data = {
+  token: process.env.SLACK_AUTH_TOKEN,  
   trigger_id: req.trigger_id,
   dialog: {
     callback_id: "ryde-46e2b0",
