d1195b7eade3a520ca64fee2eb7858fec5cc728d
Mikhail Usov <mikhail.usov@appdynamics.com>
2019-02-07 14:22:21 -0800

    formatting

--- index.js
+++ index.js
@@ -460,9 +460,8 @@ app.post('/actions', (req, res) => {
             }
           ];
     var data = {
+      channel: user.id,
       message: {
-          token: process.env.SLACK_AUTH_TOKEN,
-          channel: user.id,
           text: `Hello ${name}! You have been matched with someone who shares the following interests as you: ${interest1}, ${interest2}, ${interest3}. Please accept or decline this match.`,
           attachments: attachments
         }
