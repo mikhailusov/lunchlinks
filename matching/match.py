3bf90115ec49be7a5383e1d5379d0a89685962ec
Michael Ma <michael.ma@appdynamics.com>
2019-02-07 22:23:34 -0800

    add interest feature

--- matching/match.py
+++ matching/match.py
@@ -8,6 +8,7 @@ pp = pprint.PrettyPrinter(indent=4)
 
 CURR_USER_ID = "UFZRUAEUC"
 CURR_USER_NAME = "Michael Ma"
+INTEREST_KEYWORDS = ["photography", "boba", "pho", "drones"]
 
 slack_token = os.environ["SLACK_API_TOKEN"]
 sc = SlackClient(slack_token)
@@ -16,7 +17,6 @@ user_id_to_name_dict = {}
 user_id_list = []
 
 def normalize_dict(dict):
-
     the_min = min(dict.values())
     the_max = max(dict.values())
 
@@ -25,6 +25,34 @@ def normalize_dict(dict):
         dict[key] = normalized
 
     return dict
+
+# TODO: currently interest words are hard-coded. Need to take these keywords from UI
+# award points for people who mention your selected interest keywords
+def get_interest_score():
+    interest_score_dict = {}
+    channels = sc.api_call("channels.list")['channels']
+
+    for channel in channels:
+        curr_channel_id = channel['id']
+        curr_channel_history = sc.api_call("channels.history", channel=curr_channel_id)
+        messages = curr_channel_history['messages']
+
+        for message in messages:
+            # bots don't have a user key
+            if 'user' not in message:
+                continue
+
+            text = message['text']
+            user_id = message['user']
+
+            for word in INTEREST_KEYWORDS:
+                if word in text:
+                    if user_id not in interest_score_dict:
+                        interest_score_dict[user_id] = 1
+                    else:
+                        interest_score_dict[user_id] += 1
+
+    return(normalize_dict(interest_score_dict))
 
 
 # award points for people who are in the same channel as you
@@ -47,6 +75,7 @@ def get_channel_score():
                     channel_score_dict[member_id] += 1
     return normalize_dict(channel_score_dict)
 
+
 # award points for when you reply to someone or when others reply to you
 def get_reply_score():
     reply_score_dict = {}
@@ -54,9 +83,7 @@ def get_reply_score():
 
     for channel in channels:
         curr_channel_id = channel['id']
-
         curr_channel_history = sc.api_call("channels.history", channel=curr_channel_id)
-
         messages = curr_channel_history['messages']
 
         # points for people who reply to you
@@ -78,23 +105,33 @@ def get_reply_score():
                 else:
                     reply_score_dict[parent_id] += 1
 
-    # print(reply_score_dict)
     return(normalize_dict(reply_score_dict))
 
 
-def combine_dictionaries(d1, d2):
-    d3 = {}
-    weight1 = 0.5
-    weight2 = 0.5
+def translate_dictionary(dict):
+    new_dict = {}
+    for key, value in dict.iteritems():
+        user_name = user_id_to_name_dict[key]
+        new_dict[user_name] = dict[key]
+    return new_dict
+
+
+def combine_dictionaries(d1, d2, d3):
+    combined_dict = {}
+    weight1 = 0.3
+    weight2 = 0.3
+    weight3 = 0.4
 
     for k in d1:
-        d3[k] = d1[k] * weight1
+        combined_dict[k] = weight1 * d1[k]
         if k in d2:
-            d3[k] += weight2 * d2[k]
+            combined_dict[k] += weight2 * d2[k]
+        if k in d3:
+            combined_dict[k] += weight3 * d3[k]
+    return combined_dict
 
-    return d3
 
-def run():
+def find_best_match():
     slack_users_list = sc.api_call("users.list")['members']
 
     for user in slack_users_list:
@@ -104,21 +141,26 @@ def run():
         except KeyError:
             continue
 
+    # build all feature dictionaries
     channel_score_dict = get_channel_score()
     reply_score_dict = get_reply_score()
+    interest_score_dict = get_interest_score()
 
+    # get aggregate dictionary from all the feature dictionaries
+    combined_dict = combine_dictionaries(channel_score_dict, reply_score_dict, interest_score_dict)
 
-    final_dict = combine_dictionaries(channel_score_dict, reply_score_dict)
-    desc_order_ids = sorted(final_dict.items(), key=operator.itemgetter(1), reverse=True)
+    final_dict = translate_dictionary(combined_dict)
 
-    best_match_id = desc_order_ids[1][0]
-    print(best_match_id)
+    print(final_dict)
 
-    return(user_id_to_name_dict[best_match_id])
+    desc_order_tuples = sorted(final_dict.items(), key=operator.itemgetter(1), reverse=True)
 
+    ranked_user_names = [tup[0] for tup in desc_order_tuples]
 
+    print(ranked_user_names)
+    return ranked_user_names
 
-run()
+find_best_match()
 
 
 
